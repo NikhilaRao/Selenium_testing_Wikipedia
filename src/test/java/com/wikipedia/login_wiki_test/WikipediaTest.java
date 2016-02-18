d72d7ad469e552ad372a2e5f03d88a361c26d151
Nikhila Rao <nikhila.a.rao@gmail.com>
2016-02-18 00:43:31 -0800

    adding negetive test scenario

--- src/test/java/com/wikipedia/login_wiki_test/WikipediaTest.java
+++ src/test/java/com/wikipedia/login_wiki_test/WikipediaTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import javax.sql.rowset.serial.SerialArray;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,74 +14,100 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class WikipediaTest{
+public class WikipediaTest {
 
 	WebDriver driver;
-	
+
+	@Ignore
 	@Test
 	public void wikiSignIn() {
-		
+
 		driver = new FirefoxDriver();
-		
-		//open wikipedia webpage
+
+		// open wikipedia webpage
 		driver.get("https://www.wikipedia.org/");
-		
-		//verify if the page has openend
+
+		// verify if the page has openend
 		String response = driver.getPageSource();
-        assertTrue("Validation FAILED -Text \" Wikipedia\" NOT FOUND",
-                response.contains("Wikipedia"));
+		assertTrue("Validation FAILED -Text \" Wikipedia\" NOT FOUND", response.contains("Wikipedia"));
 
-        //select language--english
-        WebElement selectEngligh = driver.findElement(By.xpath("//a[contains(@href, '//en.wikipedia.org')] "));
-        selectEngligh.click();
-        
-        //login
-        WebElement loginButton = driver.findElement(By.linkText("Log in"));
-        loginButton.click();        
-        
-        WebElement userName = driver.findElement(By.id("wpName1"));
-        userName.clear();
-        userName.sendKeys("NikhilaRao");
-        
-        WebElement password = driver.findElement(By.id("wpPassword1"));
-        password.clear();
-        password.sendKeys("********");
-        
-        WebElement login = driver.findElement(By.id("wpLoginAttempt"));
-        login.click();
-        
-        //research about wikipedia
-        WebElement searchBox = driver.findElement(By.id("searchInput"));
-        searchBox.clear();
-        searchBox.sendKeys("Northern Lights");
+		// select language--english
+		WebElement selectEngligh = driver.findElement(By.xpath("//a[contains(@href, '//en.wikipedia.org')] "));
+		selectEngligh.click();
 
-        WebElement search = driver.findElement(By.id("searchButton"));
-        search.click();
-        
-        response = driver.getPageSource();
-        //WebElement headingText = driver.findElement(By.xpath( "\\h1[contains(text(), 'Wikipedia')]" ) );
-        assertTrue("Validation FAILED -Text \" Northern lights\" NOT FOUND",
-                response.contains("Northern lights"));
-        
-        //logout
-        WebElement logout = driver.findElement(By.linkText("Log out"));
-        logout.click();
-        
-        response = driver.getPageSource();
-        assertTrue("Validation FAILED -Text \" Log out\" NOT FOUND",
-                response.contains("Log out"));
-        
-        driver.close();
+		// login
+		WebElement loginButton = driver.findElement(By.linkText("Log in"));
+		loginButton.click();
+
+		WebElement userName = driver.findElement(By.id("wpName1"));
+		userName.clear();
+		userName.sendKeys("NikhilaRao");
+
+		WebElement password = driver.findElement(By.id("wpPassword1"));
+		password.clear();
+		password.sendKeys("********");
+
+		WebElement login = driver.findElement(By.id("wpLoginAttempt"));
+		login.click();
+
+		// research about wikipedia
+		WebElement searchBox = driver.findElement(By.id("searchInput"));
+		searchBox.clear();
+		searchBox.sendKeys("Northern Lights");
+
+		WebElement search = driver.findElement(By.id("searchButton"));
+		search.click();
+
+		response = driver.getPageSource();
+		// WebElement headingText = driver.findElement(By.xpath(
+		// "\\h1[contains(text(), 'Wikipedia')]" ) );
+		assertTrue("Validation FAILED -Text \" Northern lights\" NOT FOUND", response.contains("Northern lights"));
+
+		// logout
+		WebElement logout = driver.findElement(By.linkText("Log out"));
+		logout.click();
+
+		response = driver.getPageSource();
+		assertTrue("Validation FAILED -Text \" Log out\" NOT FOUND", response.contains("Log out"));
+
+		driver.close();
 	}
-	
+
 	@Test
-	public void LoginFail(){
-		
-		//driver = new FirefoxDriver();
-		
-		//login fail-- password field left empty
-		
-		
+	public void LoginFail() {
+
+		driver = new FirefoxDriver();
+		// open wikipedia webpage
+		driver.get("https://www.wikipedia.org/");
+
+		// verify if the page has openend
+		String response = driver.getPageSource();
+		assertTrue("Validation FAILED -Text \" Wikipedia\" NOT FOUND", response.contains("Wikipedia"));
+
+		// select language--english
+		WebElement selectEngligh = driver.findElement(By.xpath("//a[contains(@href, '//en.wikipedia.org')] "));
+		selectEngligh.click();
+
+		// login
+		WebElement loginButton = driver.findElement(By.linkText("Log in"));
+		loginButton.click();
+
+		WebElement userName = driver.findElement(By.id("wpName1"));
+		userName.clear();
+		userName.sendKeys("NikhilaRao");
+
+		WebElement password = driver.findElement(By.id("wpPassword1"));
+		password.clear();
+		password.sendKeys(" ");
+
+		WebElement login = driver.findElement(By.id("wpLoginAttempt"));
+		login.click();
+
+		String errorResponse = driver.getPageSource();
+		assertTrue("Validation FAILED -Text \" Login error\" NOT FOUND", 
+					errorResponse.contains("Login error"));
+
 	}
+
 	
 }
